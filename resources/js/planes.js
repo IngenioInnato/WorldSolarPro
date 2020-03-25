7a479399f6754c21e6e86da268a3575f00c8fcdd
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-03-25 03:38:06 -0400

    Agregada página de identidad, con detalles

---
+++ resources/js/planes.js
@@ -0,0 +1,5 @@
+var carouselFinanciadoras = document.querySelector('.carousel--financiadoras');
+var carousel = new Carousel(carouselFinanciadoras, true, false, [1,1,1,3,3]);
+
+var carouelProductos = document.querySelector('.carousel--productos');
+var carousel = new Carousel(carouelProductos,true, false, [1, 1, 1, 3, 3]);

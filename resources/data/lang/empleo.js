015966f3fa3299201b153c1cf1bb0f92ee05dfd9
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-03-25 06:08:09 -0400

    Agregada página de carrer, con detalles

---
+++ resources/data/lang/empleo.js
@@ -0,0 +1,115 @@
+var lang = [];
+// Añadir idioma
+function addLang(el){
+  el.forEach(elm => lang.push(elm)); return true;
+}
+// =============
+// Obtener idioma del navegador
+let idioma = window.navigator.languages ? window.navigator.languages[0] : null;idioma = idioma || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
+let userLang = idioma;
+if (userLang.indexOf('-') !== -1) userLang = userLang.split('-')[0];
+if (userLang.indexOf('_') !== -1) userLang = userLang.split('_')[0];
+// ============================
+const TitleText = [
+  {
+    'es': 'EMPLEO | WORLD SOLAR PRO',
+    'en': 'CARRER | WORLD SOLAR PRO'
+  }
+]; 
+// Title
+addLang(TitleText);
+
+// Menu
+addLang(MenuLang);
+
+const S1Text = [
+  {
+    'es': 'Siempre nos encontramos evaluando personal nuevo para nuestra empresa, la cual ya se encuentra en los mercados de Puerto Rico y en los estados de Florida e Illinois. Por tal razón, si reside en cualquiera de estas jurisdicciones o en las cercanías de, por favor someta su resume actualizado con todas sus experiencias de trabajo, incluyendo las certificaciones relacionadas a la industria solar, como por ejemplo de electricista, eficiencia energética, ingeniería, diseño y de instalador solar.',
+    'en': 'We are always considering new personnel for our company, which is already operating in the markets of Puerto Rico and the states of Florida and Illinois. For this reason, if you reside within or nearby any of these jurisdictions, please submit your updated resume with all of your past job experiences, including solar industry-related certifications such as electrician, energy efficiency, engineering, design and solar installer for example.'
+  }
+];
+
+const S2Text = [
+  {
+    'es': '¡Forma Parte de Nuestro Equipo!',
+    'en': 'Join Our Team !'
+  },
+  {
+    'es': 'Nombre',
+    'en': 'Name'
+  },
+  {
+    'es': 'Apellido',
+    'en': 'Last Name'
+  },
+  {
+    'es': 'Ciudad',
+    'en': 'City'
+  },
+  {
+    'es': 'Número Telefónico',
+    'en': 'Phone Number'
+  },
+  {
+    'es': 'ENVIAR',
+    'en': 'SEND'
+  }
+];
+addLang(S2Text);
+
+// Sección 3 Información
+const S3Text = [
+  {
+    'es': 'Envia tu CV a:',
+    'en': 'Send us your resume to:'
+  },
+  {
+    'es': 'Illinois: careeril@worldsolarpro.com',
+    'en': 'Illinois: careeril@worldsolarpro.com '
+  },
+  {
+    'es': 'Florida: careerfl@worldsolarpro.com',
+    'en': 'Florida: careerfl@worldsolarpro.com'
+  },
+  {
+    'es': 'Puerto Rico: reclutamiento@worldsolarpro.com',
+    'en': 'Puerto Rico: reclutamiento@worldsolarpro.com'
+  },
+];
+addLang(S3Text);
+
+addLang(FooterLang);
+var html = document.querySelector('html');
+var textItem = document.querySelectorAll('[name="text"]');
+
+if(userLang == 'es'){
+  textItem.forEach((el, i) => el.innerHTML = lang[i].es);
+  html.setAttribute('lang', 'es');
+}
+else if (userLang == 'en'){
+  textItem.forEach((el, i) => el.innerHTML = lang[i].en);
+  html.setAttribute('lang', 'en');
+}
+
+else{ 
+  textItem.forEach((el, i) => el.innerHTML = lang[i].en);
+  html.setAttribute('lang', 'en');
+}
+
+let btnFlag = document.querySelector('#flag');
+btnFlag.addEventListener('click',function(){   
+  var status = btnFlag.dataset.lang;
+  btn = btnFlag.querySelector('.flag')
+  if(status === 'es'){                                    
+    textItem.forEach((el, i) => el.innerHTML = lang[i].en);
+    btnFlag.dataset.lang = 'en';
+    btn.classList.replace('flag-en', 'flag-es');
+    html.setAttribute('lang', 'en');
+
+  } else{
+    textItem.forEach((el, i) => el.innerHTML = lang[i].es);
+    btnFlag.dataset.lang = 'es';
+    btn.classList.replace('flag-es', 'flag-en');
+    html.setAttribute('lang', 'es');
+  }
+});
---
+++ resources/js/empleo.js
@@ -0,0 +1,2 @@
+var carouselheader = document.querySelector('.carousel--header');
+var carousel = new Carousel(carouselheader, true, false, [1,1,1,2,2 ]);

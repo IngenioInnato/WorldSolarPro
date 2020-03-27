4d54ca6aa467dbe51b25e902519ec26a931f370f
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-03-27 02:00:47 -0400

    creando pagina 404

--- routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-$router->get('season-temp','/inicio.html');
-
-$router->get('season-temp/about','conocenos.html');
-
-$router->get('season-temp/services','planes.html');
-
-$router->get('season-temp/contact','contacto.html');

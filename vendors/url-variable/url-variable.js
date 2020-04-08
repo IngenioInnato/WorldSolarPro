0f4f8d864e00d6450fe701d0582a648dcc4853f3
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-04-08 04:35:27 -0400

    Mejoras añadidas

--- vendors/url-variable/url-variable.js
+++ vendors/url-variable/url-variable.js
@@ -2,6 +2,6 @@ function getUrlVars(location) {
   var url_string = window.location.href.toString(); //window.location.href
   var url = new URL(url_string);
   var result = url.searchParams.get(location);
-  console.log(result);
+  // console.log(result);
   return result;
 }

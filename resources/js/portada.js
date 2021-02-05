8ee2d597a2eb732c17da74d50188b986c5d5aac4
MiguelAngel0201 <mpena.creactiva@gmail.com>
2021-02-05 16:26:30 -0400

    Mejoras añadidas

--- resources/js/portada.js
+++ resources/js/portada.js
@@ -19,33 +19,33 @@ function getCoverData(data){
 }
 form.addEventListener('submit', e => {
   e.preventDefault();
-  // let data = getData();
-  // btnQ.disabled = true;
-  // form.innerHTML = '<h2 class="text-light text-center p-3 bg-orange">Wait a moment please...<h2>';
-  // Email.send({
-  //   SecureToken: "8a9ce791-d13b-44b0-8824-258b13a28660",
-  //   To: 'worldsolarpropr@gmail.com',
-  //   From: "leads@worldsolarprous.com",
-  //   Subject: "Datos para tax credit",
-  //   Body: `
-  //   <h1>Datos dados por el usuario</h1>
-  //   <p>Nombre: ${data.name}. <br>
-  //   Teléfono: ${data.number}. <br>
-  //   Email: ${data.email}. <br>
-  //   Dirección: ${data.address}. <br>
-  //   Ciudad: ${data.country}. <br>
-  //   Average: ${data.average}. <br>
-  //   </p>
-  //   `
-  // }).then(
-  //   message => {
-  //     console.log("Mensaje Enviado Correctamente " + message)
-  //     form.innerHTML = '<h2 class="text-light text-center p-3 bg-orange">THANKS FOR SUBMIT<h2>';
-  //   }
-  // ).catch(
-  //   message => console.log("Error: " + message)
-  // );
-  // let dataRes = getCoverData(data);
+  let data = getData();
+  btnQ.disabled = true;
+  form.innerHTML = '<h2 class="text-light text-center p-3 bg-orange">Wait a moment please...<h2>';
+  Email.send({
+    SecureToken: "8a9ce791-d13b-44b0-8824-258b13a28660",
+    To: 'worldsolarpropr@gmail.com',
+    From: "leads@worldsolarprous.com",
+    Subject: "Datos para tax credit",
+    Body: `
+    <h1>Datos dados por el usuario</h1>
+    <p>Nombre: ${data.name}. <br>
+    Teléfono: ${data.number}. <br>
+    Email: ${data.email}. <br>
+    Dirección: ${data.address}. <br>
+    Ciudad: ${data.country}. <br>
+    Average: ${data.average}. <br>
+    </p>
+    `
+  }).then(
+    message => {
+      console.log("Mensaje Enviado Correctamente " + message)
+      form.innerHTML = '<h2 class="text-light text-center p-3 bg-orange">THANKS FOR SUBMIT<h2>';
+    }
+  ).catch(
+    message => console.log("Error: " + message)
+  );
+  let dataRes = getCoverData(data);
   // console.log(dataRes);
   // if( btnFlag.dataset.lang == 'es'){
   //   footerForm.innerHTML = '<h2 class="text-light text-center p-3 bg-orange">GRACIAS POR ENVIAR SU MENSAJE<h2>';
